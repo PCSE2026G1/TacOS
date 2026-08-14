49777d923694b3ca195596c855ba24eb02b2c88d
i20hayasi <i20hayasi@tokuyama.kosen-ac.jp>
2026-08-14 17:26:58 +0900

    lcd_init

--- os/app/src/lcd.h
+++ os/app/src/lcd.h
@@ -3,8 +3,25 @@
 
 #include "cmmdef.h"
 
+#ifndef CMMDEF
+typedef struct
+{
+    unsigned int buf[512];
+    unsigned int flags[8];
+} lcd_t;
+#endif
+
 extern void lcd_draw_string(unsigned int x, unsigned int y, const char PTR(str));
 extern void lcd_set_display(int display);
 extern int lcd_get_display(VOID);
+
+extern void lcd_init(STRUCT(lcd_t) PTR(lcd));
+extern void lcd_flush(STRUCT(lcd_t) PTR(lcd));
+extern void lcd_draw(STRUCT(lcd_t) PTR(lcd),
+        unsigned int x, unsigned int y, int value);
+extern void lcd_draw_rect(STRUCT(lcd_t) PTR(lcd),
+        unsigned int x, unsigned int y, unsigned int width, unsigned int height, int value);
+extern void lcd_fill_rect(STRUCT(lcd_t) PTR(lcd),
+        unsigned int x, unsigned int y, unsigned int width, unsigned int height, int value);
 
 #endif
